80c1c8c6174245455fcc31f5ac29f5365b93df82
Henrique-777 <pedroalvescruz2000@gmail.com>
2025-09-22 21:16:46 -0300

    Inicio da tela de login inspirada em design do Figma

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Container, ContainerLeft, ContainerRight, Subtitle, ContainerForm, StyledButton } from "./loginStyles.js";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const Login = () => {
     return (
-        <div>
-            <h1> LOGIN TELA INICIO </h1>
-        </div>
+        <Container>
+            <ContainerLeft>  </ContainerLeft>
+            <ContainerRight> 
+                Welcome 
+                <Subtitle>Please login here</Subtitle>
+                <ContainerForm>
+                    <TextField
+                    required
+                    id="email"
+                    label="Endereço de e-mail"
+                    defaultValue=""
+                    />
+                    <TextField
+                    required
+                    id="senha"
+                    label="Senha"
+                    defaultValue=""
+                    />
+                </ContainerForm>
+                <Stack>
+                    <StyledButton variant="contained">Login</StyledButton>
+                </Stack>
+            </ContainerRight>
+
+        </Container>
     );
 };
 
